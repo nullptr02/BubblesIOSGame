ff1f90579442317bbdd50960489c9500df3931c3
nullptr02 <mesteves@my.yorku.ca>
2022-05-06 14:09:58 -0400

    Completed the Controllers for the different modules

--- Bubbles/Controller/HighScoreViewController.swift
+++ Bubbles/Controller/HighScoreViewController.swift
@@ -10,4 +10,47 @@ import UIKit
 
 class HighScoreViewController: UIViewController {
     
+    @IBOutlet weak var highScoreTableView: UITableView!
+    
+    let userDefaults = UserDefaults()
+    var nameScore = [String: Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if userDefaults.value(forKey: "playerName") == nil {
+            userDefaults.setValue("ANONYMOUS", forKey: "playerName")
+        }
+        if userDefaults.value(forKey: "leaderboard") != nil {
+            nameScore = userDefaults.value(forKey: "leaderboard") as! [String: Int]
+        }
+        
+        
+    }
+    
+    @IBAction func returnMainPage(_ sender: UIButton) {
+        
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+}
+
+extension HighScoreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameScore.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "highScoreCell", for: indexPath)
+        
+        let sortOne = nameScore.sorted {
+            (first, second) -> Bool in
+            return first.value > second.value
+        }
+        
+        cell.textLabel?.text = "\(sortOne[indexPath.row].key)"
+        cell.detailTextLabel?.text = "\(sortOne[indexPath.row].value)"
+        
+        return cell
+    }
 }
--- Bubbles/Controller/SettingViewController.swift
+++ Bubbles/Controller/SettingViewController.swift
@@ -8,6 +8,89 @@
 import Foundation
 import UIKit
 
-class SettingViewController: UIViewController, UITextViewDelegate {
+class SettingViewController: UIViewController, UITextFieldDelegate {
+    
+    let userDefaults = UserDefaults()
+    
+    @IBOutlet weak var playerName: UITextField!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var noBubblesLabel: UILabel!
+    @IBOutlet weak var timerSlider: UISlider!
+    @IBOutlet weak var maxBubbleSlider: UISlider!
+    
+    @IBAction func closeSettingsButton(_ sender: Any) {
+        self.removeAnimation()
+    }
+    
+    @IBAction func maxTimeSlider(_ sender: UISlider) {
+        
+        var maxTime = Int(sender.value)
+        userDefaults.setValue(maxTime, forKey: "gameTime")
+        timeLabel.text = String(maxTime) + "s"
+        
+    }
+    
+    @IBAction func maxBubblesSlider(_ sender: UISlider) {
+        var maxBubbles = Int(sender.value)
+        userDefaults.setValue(maxBubbles, forKey: "noBubbles")
+        noBubblesLabel.text = String(maxBubbles)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        self.showAnimation()
+        playerName.delegate = self
+        
+        if let value = userDefaults.value(forKey: "playerName") as? String {
+            playerName.text = value
+        }
+        
+        if let value = userDefaults.value(forKey: "gameTime") as? Float {
+            timerSlider.value = value
+        }
+        
+        if let value = userDefaults.value(forKey: "gameTime") as? Int {
+            timeLabel.text = String(value) + "s"
+        }
+        
+        if let value = userDefaults.value(forKey: "noBubbles") as? Float {
+            maxBubbleSlider.value = value
+        }
+        
+        if let value = userDefaults.value(forKey: "noBubbles") as? Int {
+            noBubblesLabel.text = String(value)
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userDefaults.setValue(playerName.text, forKey: "playerName")
+        playerName.resignFirstResponder()
+        return true
+    }
+    
+    func showAnimation() {
+        
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimation() {
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0
+        }, completion: {(finished : Bool) in
+            if(finished) {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
     
 }
